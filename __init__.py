e8bd20b358ba76fada0a7a999f5927f16953d286
deustenebrarum <gamesevolutionteam@gmail.com>
2024-02-14 19:20:15 +0300

    Fix module imports and add setup.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from .database_context import DBContext
 from .database_context import Table
-import sqlite_orm.column_types as column_types
+import column_types
+
 
 __all__ = [
     'DBContext',
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='simple-sqlite-orm',
+   version='1.0',
+   description='',
+   author='deustenebrarum',
+   author_email='gamesevolutionteam@gmail.com',
+   packages=['simple-sqlite-orm'],
+)
